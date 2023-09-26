a09f2b3ada17ef7f2d6fa6906f198d367b80b066
Mihai Paun <email.mihu@gmail.com>
2023-09-26 23:40:22 +0200

    Created models for Auction Listings and their Category.

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,25 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
+# User model
 class User(AbstractUser):
     pass
+
+# Category model
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+# Auction Listing model
+class Listing(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    date_time_creation = models.DateTimeField(auto_now_add=True)
+    picture = models.URLField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name="listings")
+
+    def __str__(self):
+        return self.name
+    
