99205366decfdaff856036dcef3dd8f3544edc8d
Mihai Paun <email.mihu@gmail.com>
2023-09-27 23:56:28 +0200

    Defined the basic strcture of the modules required in the task.

--- auctions/models.py
+++ auctions/models.py
@@ -5,21 +5,38 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-# Category model
+""" # Category model
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
+ """
 
 # Auction Listing model
 class Listing(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=9, decimal_places=2)
+    title = models.CharField(max_length=255)
+    description = models.TextField
+    starting_price = models.DecimalField(max_digits=9, decimal_places=2)
+    curr_price = models.DecimalField(max_digits=9, decimal_places=2)
     date_time_creation = models.DateTimeField(auto_now_add=True)
     picture = models.URLField(null=True, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name="listings")
+    """ category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name="listings") """
+    category = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
         return self.name
     
+# Bid model
+class Bid(models.Model):
+    amount = models.DecimalField(max_digits=9, decimal_places=2)
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    date_time_creation = models.DateTimeField(auto_now_add=True)
+
+# Comment model
+class Comment(models.Model):
+    comm = models.TextField
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    date_time_creation = models.DateTimeField(auto_now_add=True)   
