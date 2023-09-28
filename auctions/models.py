d5fe9e65c10ee95b32ea5bf8f295cabe1ddf1694
Mihai Paun <email.mihu@gmail.com>
2023-09-28 23:45:03 +0200

    Made the highest bid update listing.curr_price

--- auctions/models.py
+++ auctions/models.py
@@ -11,10 +11,16 @@ class Listing(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField
     starting_price = models.DecimalField(max_digits=9, decimal_places=2)
-    curr_price = models.DecimalField(max_digits=9, decimal_places=2)
+    curr_price = models.DecimalField(max_digits=9, decimal_places=2, default=0.00)
     date_time_creation = models.DateTimeField(auto_now_add=True)
     picture = models.URLField(null=True, blank=True)
     category = models.CharField(max_length=255, blank=True)
+
+    # change the save method so that we can set curr_price = start_price
+    def save(self, *args, **kwargs):
+        if self.curr_price == 0.00:
+            self.curr_price = self.starting_price
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
@@ -23,8 +29,22 @@ class Listing(models.Model):
 class Bid(models.Model):
     amount = models.DecimalField(max_digits=9, decimal_places=2)
     bidder = models.ForeignKey(User, on_delete=models.CASCADE)
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='bids')
     date_time_creation = models.DateTimeField(auto_now_add=True)
+
+    # change the save method so that we can update the curr price
+    # of a listing if this is the highest bid on that listing
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        # find the highest bid on that particular listing
+        highest_bid = Bid.objects.filter(listing=self.listing).order_by('-amount').first()
+        if highest_bid:
+            self.listing.curr_price = highest_bid.amount
+            self.listing.save()
+
+    def __str__(self):
+        return str(self.amount)
 
 # Comment model
 class Comment(models.Model):
