cf36b4682434ed44e254f58f3b3f635203b6ef5a
Mihai Paun <email.mihu@gmail.com>
2023-09-28 13:41:11 +0200

    Removed the commented Category model and the old category field in the Listing model.

--- auctions/models.py
+++ auctions/models.py
@@ -5,14 +5,6 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-""" # Category model
-class Category(models.Model):
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
- """
-
 # Auction Listing model
 class Listing(models.Model):
     title = models.CharField(max_length=255)
@@ -21,7 +13,6 @@ class Listing(models.Model):
     curr_price = models.DecimalField(max_digits=9, decimal_places=2)
     date_time_creation = models.DateTimeField(auto_now_add=True)
     picture = models.URLField(null=True, blank=True)
-    """ category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name="listings") """
     category = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
